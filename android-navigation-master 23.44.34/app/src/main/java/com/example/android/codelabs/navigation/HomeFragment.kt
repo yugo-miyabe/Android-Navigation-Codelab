83afa672f356a98aa9909fd54de8c05029db71d5
yugo <miyabe@fenrir.co.jp>
2022-09-12 00:38:37 +0900

    アニメーション追加

--- android-navigation-master 23.44.34/app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ android-navigation-master 23.44.34/app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -25,6 +25,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 
 /**
  * Fragment used to show how to navigate to another destination
@@ -50,17 +51,17 @@ class HomeFragment : Fragment() {
         //TODO END STEP 5
 
         //TODO STEP 6 - Set NavOptions
-//        val options = navOptions {
-//            anim {
-//                enter = R.anim.slide_in_right
-//                exit = R.anim.slide_out_left
-//                popEnter = R.anim.slide_in_left
-//                popExit = R.anim.slide_out_right
-//            }
-//        }
-//        view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
-//            findNavController().navigate(R.id.flow_step_one_dest, null, options)
-//        }
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+        view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null, options)
+        }
         //TODO END STEP 6
 
         //TODO STEP 7.2 - Update the OnClickListener to navigate using an action
